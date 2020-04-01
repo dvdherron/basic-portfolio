fe5ca2f6d2416215504afc2415d272c6ba7d5991
dvdherron <dvdherron@gmail.com>
2020-04-01 18:46:51 +0200

    first styles for nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,19 +1,22 @@
 import React from "react";
 import Link from "next/link";
+import "./Nav.scss";
 
 const Nav = ({ settings }) => (
-  <nav>
-    <ul>
-      {settings &&
-        settings.content.main_nav.map((navitem, index) => (
-          <li key={index}>
-            <Link href={navitem.link.cached_url}>
-              <a>{navitem.name}</a>
-            </Link>
-          </li>
-        ))}
-    </ul>
-  </nav>
+  <div className="nav--container">
+    <nav role="navigation">
+      <ul>
+        {settings &&
+          settings.content.main_nav.map((navitem, index) => (
+            <li key={index}>
+              <Link href={navitem.link.cached_url}>
+                <a>{navitem.name}</a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Nav;
--- components/layout.js
+++ components/layout.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Head from "../components/head";
-import Nav from "../components/nav";
+import Nav from "../components/Nav";
 import StoryblokService from "../utils/storyblok-service";
 
 export default ({ children, settings = {} }) => (
   <div>
     <Head />
-    <Nav settings={settings} />
-    <div className="util__container">{children}</div>
+    <div className="util__container">
+      <Nav settings={settings} />
+      {children}
+    </div>
 
     {StoryblokService.bridge()}
   </div>
