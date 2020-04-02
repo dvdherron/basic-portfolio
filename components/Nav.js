e4a7f4543432318580ab0a8f3e19676d76bf66ff
dvdherron <dvdherron@gmail.com>
2020-04-02 08:21:08 +0200

    fix nav alignment

--- components/Nav.js
+++ components/Nav.js
@@ -3,20 +3,21 @@ import Link from "next/link";
 import "./Nav.scss";
 
 const Nav = ({ settings }) => (
-  <div className="nav--container">
-    <nav role="navigation">
-      <ul>
-        {settings &&
-          settings.content.main_nav.map((navitem, index) => (
-            <li key={index}>
-              <Link href={navitem.link.cached_url}>
-                <a>{navitem.name}</a>
-              </Link>
-            </li>
-          ))}
-      </ul>
-    </nav>
-  </div>
+  <nav className="main-nav" role="navigation">
+    <ul>
+      <Link href="/">
+        <a>home</a>
+      </Link>
+      {settings &&
+        settings.content.main_nav.map((navitem, index) => (
+          <li key={index}>
+            <Link href={navitem.link.cached_url}>
+              <a>{navitem.name}</a>
+            </Link>
+          </li>
+        ))}
+    </ul>
+  </nav>
 );
 
 export default Nav;
